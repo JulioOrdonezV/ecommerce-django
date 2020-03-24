0e8862338f24195e543da562b5f682dc2c4f5c62
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-03-24 16:59:44 -0600

    Add the route to the shopping cart functionality to the urls

--- ecommerce/cookie_store/urls.py
+++ ecommerce/cookie_store/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from cookie_store.views import item_detail
+from cookie_store.views import item_detail, add_to_cart
 
 app_name = 'cookie_store'
 
 urlpatterns = [
     path('', item_detail, name='item-detail'),
+    path('add-to-cart/<pk>', add_to_cart, name='add-to-cart'),
 ]
