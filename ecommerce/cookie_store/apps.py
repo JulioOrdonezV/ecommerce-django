80e466798a26b8edd2bee96c186cd789a2ba3a05
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-03-24 10:55:17 -0600

    First commit

---
+++ ecommerce/cookie_store/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CookieStoreConfig(AppConfig):
+    name = 'cookie_store'
---
+++ ecommerce/cookie_store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    fecha_inicio = models.DateTimeField(auto_now_add=True)
+    fecha_completada = models.DateTimeField()
+    completada = models.BooleanField(default=False)
+    items = models.ManyToManyField(OrderItem)
