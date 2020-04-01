12d317186901170143742681a4b1fe9386084b71
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-03-31 21:27:04 -0600

    Refactor cart_tags product-page and other to use the new attributes names

--- ecommerce/cookie_store/templatetags/cart_tags.py
+++ ecommerce/cookie_store/templatetags/cart_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.simple_tag()
 def cart_item_count():
-    order_qs = Order.objects.filter(completada=False)
+    order_qs = Order.objects.filter(completed=False)
     if order_qs.exists():
-        return order_qs[0].cantidad
+        return order_qs[0].quantity
     return 0
